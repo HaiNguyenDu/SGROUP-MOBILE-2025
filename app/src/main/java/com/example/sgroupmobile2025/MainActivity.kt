2bbbea04778c200d5f438ed68e85a376b0dfd49a
YenNu1402 <yennu1422005@gmail.com>
2025-09-15 14:10:11 +0700

    Register

--- app/src/main/java/com/example/sgroupmobile2025/MainActivity.kt
+++ app/src/main/java/com/example/sgroupmobile2025/MainActivity.kt
@@ -5,15 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.selects.SelectInstance
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : AppCompatActivity(){
+    //    private val binding by lazy { MainActivity }
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState )
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)){v, insets ->
+            val systemsBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemsBars.left, systemsBars.top, systemsBars.right, systemsBars.bottom)
             insets
         }
     }
