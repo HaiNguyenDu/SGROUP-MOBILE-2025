a3d991a99fcefa7ab0d9c47e5751045d360a4b81
YenNu1402 <yennu1422005@gmail.com>
2025-09-15 19:49:47 +0700

    Save WIP before switching branch

--- app/src/main/java/com/example/sgroupmobile2025/MainActivity.kt
+++ app/src/main/java/com/example/sgroupmobile2025/MainActivity.kt
@@ -6,17 +6,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.selects.SelectInstance
+import android.widget.Button
+import android.content.Intent
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     //    private val binding by lazy { MainActivity }
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)){v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemsBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemsBars.left, systemsBars.top, systemsBars.right, systemsBars.bottom)
             insets
+        }
+        // Nút Sign Up để chuyển sang Register
+        val signUpButton = findViewById<Button>(R.id.button_sign_up)
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, Register_Activity::class.java)
+            startActivity(intent)
+        }
+
+        // Nút Sign In để hiện thông báo
+        val signInButton = findViewById<Button>(R.id.button_sign_in)
+        signInButton.setOnClickListener {
+            Toast.makeText(this, "Sign In clicked!", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/sgroupmobile2025/RegisterActivity.kt
+++ app/src/main/java/com/example/sgroupmobile2025/RegisterActivity.kt
@@ -1,4 +1,21 @@
 package com.example.sgroupmobile2025
 
-class RegisterActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        // Nút Back để quay về MainActivity (Login)
+        val backButton = findViewById<Button>(R.id.button_back)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // đóng RegisterActivity để không bị quay ngược bằng nút back hệ thống
+        }
+    }
 }
