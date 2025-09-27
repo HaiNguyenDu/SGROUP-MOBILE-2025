4c6dd9c505672df9f6b1d5c4a384c4e0763f29bc
T Phuong <0511tphuong@gmail.com>
2025-09-27 10:38:03 +0700

    Update UI

--- app/src/main/java/com/example/sgroupmobile2025/ProductActivity.kt
+++ app/src/main/java/com/example/sgroupmobile2025/ProductActivity.kt
@@ -19,10 +19,11 @@ class ProductActivity : AppCompatActivity() {
         setupRecyclerView()
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val inset = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val inset = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             val layout = binding.cvSearch.layoutParams
             if(layout is ViewGroup.MarginLayoutParams){
                 layout.topMargin = inset.top
+                layout.bottomMargin = inset.bottom
             }
             binding.cvSearch.layoutParams = layout
             WindowInsetsCompat.CONSUMED
@@ -36,12 +37,20 @@ class ProductActivity : AppCompatActivity() {
 
     private fun createProductList(): List<DataProduct> {
         return listOf(
-            DataProduct(R.drawable.product_1, "Converse", "Run Star Hike Three Color Unisex", 85.5),
+            DataProduct(R.drawable.product_1, "Converse", "Run Star Hike Three Color Unisex Hike Three Color Unisex", 85.5),
             DataProduct(R.drawable.product_2, "Nike", "Air Force 1 White", 120.0),
             DataProduct(R.drawable.product_3, "Adidas", "Ultraboost 22", 180.0),
             DataProduct(R.drawable.product_4, "Puma", "RS-X Toys", 95.0),
             DataProduct(R.drawable.product_1, "Vans", "Old Skool Black", 75.0),
             DataProduct(R.drawable.product_3, "Nike", "Air Force 1 Shadow Beige Pale Ivory", 115.0),
+            DataProduct(R.drawable.product_2, "Nike", "Air Force 1 White", 120.0),
+            DataProduct(R.drawable.product_3, "Adidas", "Ultraboost 22", 180.0),
+            DataProduct(R.drawable.product_4, "Puma", "RS-X Toys", 95.0),
+            DataProduct(R.drawable.product_1, "Vans", "Old Skool Black", 75.0),
+            DataProduct(R.drawable.product_2, "Nike", "Air Force 1 White", 120.0),
+            DataProduct(R.drawable.product_3, "Adidas", "Ultraboost 22", 180.0),
+            DataProduct(R.drawable.product_4, "Puma", "RS-X Toys", 95.0),
+            DataProduct(R.drawable.product_1, "Vans", "Old Skool Black", 75.0),
 
         )
     }
