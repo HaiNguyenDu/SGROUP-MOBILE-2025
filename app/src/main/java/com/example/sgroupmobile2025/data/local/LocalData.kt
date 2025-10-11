6b1db83eeeae2207e5dadf48a6ceda55cd0fd689
T Phuong <0511tphuong@gmail.com>
2025-10-11 19:03:43 +0700

    feat: Load Image from Gallery

--- app/src/main/java/com/example/sgroupmobile2025/data/local/LocalData.kt
+++ app/src/main/java/com/example/sgroupmobile2025/data/local/LocalData.kt
@@ -2,7 +2,9 @@ package com.example.sgroupmobile2025.data.local
 
 import android.content.ContentUris
 import android.content.Context
+import android.os.Build
 import android.provider.MediaStore
+import android.util.Log
 import com.example.sgroupmobile2025.data.model.Image
 
 class LocalData(private var context: Context) {
@@ -15,8 +17,8 @@ class LocalData(private var context: Context) {
             MediaStore.Images.Media.IS_PRIVATE,
             MediaStore.Images.Media.DISPLAY_NAME
         )
-        val selection = ""
-        val selectionArgs = arrayOf("")
+        val selection = null
+        val selectionArgs = null
         try{
             context.contentResolver.query(
                 uriStore,
@@ -24,19 +26,21 @@ class LocalData(private var context: Context) {
                 selection,
                 selectionArgs,
                 "${MediaStore.Images.Media.DATE_ADDED} DESC"
-            )?.let {
+            )?.use {
                 cursor ->
                 val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
                 val dateColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_ADDED)
-                val favoriteColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.IS_FAVORITE)
+//                val favoriteColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.IS_FAVORITE)
                 val nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
                 while (cursor.moveToNext()){
                     val id = cursor.getLong(idColumn)
                     val date = cursor.getLong(dateColumn)
-                    val favorite = cursor.getInt(favoriteColumn)
+//                    val isFavorite = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//                        cursor.getInt(cursor.getColumnIndexOrThrow(favoriteColumn)) == 1
+//                    } else false
                     val name = cursor.getString(nameColumn)
                     val imgSrc = ContentUris.withAppendedId(uriStore, id)
-                    images.add(Image( imgSrc, name, date, favorite == 1))
+                    images.add(Image( imgSrc, name, date, false))
                 }
             }
         }catch(e: Exception){
--- app/src/main/java/com/example/sgroupmobile2025/ui/gallery/LocalImageActivity.kt
+++ app/src/main/java/com/example/sgroupmobile2025/ui/gallery/LocalImageActivity.kt
@@ -1,21 +1,117 @@
 package com.example.sgroupmobile2025.ui.gallery
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.sgroupmobile2025.R
+import com.example.sgroupmobile2025.data.local.LocalData
+import com.example.sgroupmobile2025.databinding.ActivityLocalImageBinding
+import kotlinx.coroutines.launch
 
 class LocalImageActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLocalImageBinding.inflate(layoutInflater) }
+    val IMAGE_PERMISSION = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        Manifest.permission.READ_MEDIA_IMAGES
+    } else {
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){
+        granted ->
+        when{
+            granted ->{
+                binding.cardPermission.visibility = View.GONE
+                loadImages()
+            }
+            shouldShowRequestPermissionRationale(IMAGE_PERMISSION) -> showRationale()
+            else -> showDialogGoToSetting()
+        }
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_local_image)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val hasPermission = checkSelfPermission(IMAGE_PERMISSION) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        if (hasPermission) {
+            loadImages()
+            binding.cardPermission.visibility = View.GONE
+        } else {
+            binding.cardPermission.visibility = View.VISIBLE
+
+        }
+        binding.btnRequestPermission.setOnClickListener {
+            permissionLauncher.launch(IMAGE_PERMISSION)
+        }
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
     }
+    fun loadImages(){
+        lifecycleScope.launch {
+            val localImage = LocalData(this@LocalImageActivity)
+            val images = localImage.getImages()
+            val adapter = LocalImageAdapter(images)
+            Log.e("EEEE", images.toString())
+            binding.rvImages.layoutManager = GridLayoutManager(this@LocalImageActivity, 4)
+            binding.rvImages.adapter = adapter
+        }
+    }
+    private fun showRationale() {
+        AlertDialog.Builder(this)
+            .setTitle("Cần quyền truy cập ảnh")
+            .setMessage("App cần quyền này để hiển thị hình ảnh từ thư viện.")
+            .setPositiveButton("Đồng ý") { dialog, _ ->
+                // Yêu cầu quyền lại
+                permissionLauncher.launch(IMAGE_PERMISSION)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Hủy") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+    private fun showDialogGoToSetting() {
+        AlertDialog.Builder(this)
+            .setTitle("Quyền bị từ chối vĩnh viễn")
+            .setMessage("App cần quyền truy cập ảnh. Vui lòng bật quyền trong Cài đặt.")
+            .setPositiveButton("Đi tới Cài đặt") { dialog, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Hủy") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+
 }
